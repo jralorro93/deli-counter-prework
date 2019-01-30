e06ff66e49ebf5bf1edf7888ec6a0d08f65abdc7
Juan Alorro <jralorro93@gmail.com>
2019-01-30 20:08:09 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@ katz_deli = []
 
 def  line(katz_deli)
   if katz_deli.length == 0 
-    return "The line is currently empty"
+    return "The line is currently empty."
   else 
     return katz_deli
   end 
